68daf95559900f7b36cc07b0bdbca002d1697495
Wljones246 <Wljones246@gmail.com>
2016-10-06 18:05:27 -0400

    Added hacktoberfest with 4 blank probs

---
+++ Test practice/src/hacktoberfest16/Prob1.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package hacktoberfest16;
+
+/**
+ * @author williamjones
+ *
+ */
+public class Prob1 {
+
+	/**
+	 * 
+	 */
+	public Prob1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Test practice/src/hacktoberfest16/Prob2.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package hacktoberfest16;
+
+/**
+ * @author williamjones
+ *
+ */
+public class Prob2 {
+
+	/**
+	 * 
+	 */
+	public Prob2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
