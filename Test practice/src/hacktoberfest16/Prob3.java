d28d290ae2f155273838a7875dd80718e2a9e34f
Wljones246 <Wljones246@gmail.com>
2016-10-25 09:15:36 -0400

    Up and running

--- Test practice/src/hacktoberfest16/Prob3.java
+++ Test practice/src/hacktoberfest16/Prob3.java
@@ -3,25 +3,20 @@
  */
 package hacktoberfest16;
 
+import java.util.Scanner;
+
 /**
  * @author williamjones
  *
  */
 public class Prob3 {
 
-	/**
-	 * 
-	 */
-	public Prob3() {
-		// TODO Auto-generated constructor stub
-	}
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner Name = new Scanner(System.in);
+		System.out.println("Hello " + Name);
 	}
 
 }
